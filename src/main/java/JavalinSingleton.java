2bda51e312c398e54fe54af70a4602c51d7f66ce
Dabana Intenque <Dabanaluintenque55@gmail.com>
2024-03-27 19:15:41 +0000

    Week6 Coding Lab: Path Parameters Done

--- src/main/java/JavalinSingleton.java
+++ src/main/java/JavalinSingleton.java
@@ -19,6 +19,9 @@ public class JavalinSingleton {
         app.get("/firstname/{first}", ctx -> {
             
             //write code here
+            String userName = ctx.pathParam("first");
+
+            ctx.result(userName);
 
         });
 
